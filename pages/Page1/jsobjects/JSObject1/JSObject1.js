66192504ed417d8b8d496b51838c234b2349aac2
aparna+2 <aparna+2@appsmith.com>
2025-09-04 08:18:05 +0000

    jsobjectadded

---
+++ pages/Page1/jsobjects/JSObject1/JSObject1.js
@@ -0,0 +1,14 @@
+export default {
+	myVar1: [],
+	myVar2: {},
+	myFun1 () {
+		//	write code here
+		//	this.myVar1 = [1,2,3]
+	},
+	async myFun2 () {
+		
+		console.log("TEST")
+		//	use async-await or promises
+		//	await storeValue('varName', 'hello world')
+	}
+}
